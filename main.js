5a90e1f624e62370087895661b9f0d85c9a76bf2
Henny2 <henrieke.baunack@web.de>
2023-04-29 21:04:01 -0700

    started api call

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 import "./style.css"
 // you could also import the css within the html file via link
+import { getWeather } from "./weather"
+
+// using. then() because the function returns a promise
+getWeather(12.34, 14.53, Intl.DateTimeFormat().resolvedOptions().timeZone).then(res => {
+  console.log(res.data)
+})
+
+// stopped at 28:30
--- weather.js
+++ weather.js
@@ -1,7 +1,18 @@
 // https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=America%2FLos_Angeles
-
+import axios from "axios";
 export function getWeather(lat, lon, timezone) {
+    return axios.get("https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime",
+        {
+            params: {
 
+                latitude: lat,
+                longitude: lon,
+                timezone: timezone,
+
+
+            }
+        }
+    )
 }
 
 // stopped at 25:40
